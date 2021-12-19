2fca37b0fa31632d7540ac7437f8a220d8f88b71
TriNgo <tringo0108@gmail.com>
2021-12-19 09:07:32 +0700

    change format images

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,23 +1,23 @@
-import slider_1 from "./slider_1.jpg";
-import slider_2 from "./slider_2.jpg";
-import slider_3 from "./slider_3.jpg";
-import slider_4 from "./slider_4.jpg";
-import slider_5 from "./slider_5.jpg";
-import slider_6 from "./slider_6.png";
-import best_girl from "./best_girl.jpg";
-import in_stock from "./in_stock.jpg";
-import preorder from "./preorder.jpg";
-import genshin from "./genshin.jpg";
-import sayu from "./sayu.jpg";
-import kanojou from "./kanojou.jpg";
-import takagi from "./takagi.jpg";
-import japan from "./japan.png";
-import rating from "./rating.png";
-import free from "./free.png";
-import footer_background from"../images/footer_background.png";
-import footer_image from "./footer_image.png";
-import background from "./background.png";
-import logo from "./logo.png"
+import slider_1 from "./slider_1.avif";
+import slider_2 from "./slider_2.avif";
+import slider_3 from "./slider_3.avif";
+import slider_4 from "./slider_4.avif";
+import slider_5 from "./slider_5.avif";
+import slider_6 from "./slider_6.avif";
+import best_girl from "./best_girl.avif";
+import in_stock from "./in_stock.avif";
+import preorder from "./preorder.avif";
+import genshin from "./genshin.avif";
+import sayu from "./sayu.avif";
+import kanojou from "./kanojou.avif";
+import takagi from "./takagi.avif";
+import japan from "./japan.avif";
+import rating from "./rating.avif";
+import free from "./free.avif";
+import footer_background from"../images/footer_background.avif";
+import footer_image from "./footer_image.avif";
+import background from "./background.avif";
+import logo from "./logo.avif"
 export {
   slider_1,
   slider_2,
